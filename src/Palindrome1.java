fd2c64ad0303d05a9d7abef7ce76069fcd6ffba0
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2019-08-23 23:15:40 -0500

    Add files via upload

---
+++ src/Palindrome1.java
@@ -0,0 +1,52 @@
+//Elena Voinu
+
+import java.util.Scanner;
+
+public class Palindrome1 {
+
+    public static void main(String [] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a word to check if it's a" +
+                " palindrome or not: ");
+        String word = sc.nextLine();
+
+        reverse(word);
+
+        //check if the reversed word equals to user's input
+        if (reverse(word).equalsIgnoreCase(word)){
+            System.out.println(word.toUpperCase() + " is a palindrome");
+        }
+        else{
+            System.out.println(word.toUpperCase() + " is not a palindrome");
+        }
+
+    }
+    public static String reverse(String word){
+
+        //convert the string to an array of characters
+        char[] ch = word.toCharArray();
+
+        // create an array to store the characters
+        char[] result = new char[ch.length];
+
+        //indexes as many as in ch array
+        int len = ch.length;
+
+        //traverse the the char array in reverse direction
+        for( char c : ch){
+
+            // store the characters in the second array
+            result[len-1] = c;
+
+            // and decrement the index by 1
+            len--;
+        }
+
+         return new String(result);
+
+
+
+    }
+}
